2bad7b995f2c86bbd9932dd68b5bccb223547d47
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2024-10-21 08:45:19 +0530

    main

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import util
 
 app = Flask(__name__)
+util.load_saved_artifacts()
+
 
 
 @app.route('/classify_image', methods=['GET', 'POST'])
@@ -14,7 +16,7 @@ def classify_image():
 
     return response
 
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Sports Celebrity Image Classification")
-    util.load_saved_artifacts()
-    # app.run(port=5000)
+# if __name__ == "__main__":
+#     print("Starting Python Flask Server For Sports Celebrity Image Classification")
+#     # util.load_saved_artifacts()
+#     # app.run(port=5000)
